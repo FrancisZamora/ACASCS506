9e9d625d38b6e52bef06fec94502a002ea487cc3
FrancisZamora <franciszamor@gmail.com>
2018-11-29 16:42:04 -0500

    dividing up files

---
+++ keywordfrequency.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+import json
+
+table = defaultdict(int)
+rawjson = []
+
+name  = 'keywords.txt'
+file = open(name,'r')
+print(file)
+	
+
+print(rawjson)
+
+file.close()
