a002879158f18e7f93ba951bca650c05d7d0cf6c
FrancisZamora <franciszamor@gmail.com>
2018-11-29 15:15:10 -0500

    subset of the data key phrases writing to file"

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import boto3
 import json
 import csv
 import pandas as pd
+import sys
+
 
 #initalize AWS comprehend
 comprehend = boto3.client(service_name ='comprehend', region_name = 'us-east-1')
@@ -13,7 +15,7 @@ with open('ASRS_DBOnline.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     rows = list(reader)
     features = rows[0]
-    rows = rows[1:]
+    rows = rows[2:]
 
     for row in rows:
     	ASRS_rows.append(row)
@@ -29,26 +31,88 @@ for feature in features:
 
 
 ASRS_DF = pd.DataFrame(ASRS_rows, columns=ASRS_cols)
+ASRS_DF.columns = [c.replace(' ', '_') for c in ASRS_DF.columns]
 
 
-# get reports 
-clean_synopsis = []
-'''
-for i in ASRS_DF.iterrows():
-	clean_synopsis.append(i['Report 1'])
 
-print(clean_synopsis)    	
-'''
+reportOneNarrative = []
+reportOneCallback = []
+reportTwoNarrative = []
+reportTwoCallback = []
+reportSynopsis = []
+check = 0
+
+for a in (ASRS_DF['Report_1_Narrative']):
+	check +=1
+	if check > 50:
+		break
+	if a == '' or sys.getsizeof(a) > 4999:
+		reportOneNarrative.append('')
+	else:
+		reportOneNarrative.append(comprehend.detect_key_phrases(Text=a,LanguageCode='en')['KeyPhrases'])
+check = 0
+for b in (ASRS_DF['Report_1_Narrative']):
+	check +=1
+	if check > 50:
+		break
+	if b == '' or sys.getsizeof(b) > 4999:
+		reportOneCallback.append('')
+	else:
+		reportOneCallback.append(comprehend.detect_key_phrases(Text=b,LanguageCode='en')['KeyPhrases'])
+check = 0 
+for c in (ASRS_DF['Report_2_Narrative']):
+	check +=1
+	if check > 50:
+		break
+	if c == '' or sys.getsizeof(c) > 4999:
+		reportTwoNarrative.append('')
+	else:
+		reportTwoNarrative.append(comprehend.detect_key_phrases(Text=c,LanguageCode='en')['KeyPhrases'])
+check = 0
+for d in (ASRS_DF['Report_2_Callback']):
+	check +=1
+	if check > 50:
+		break
+	if d == '' or sys.getsizeof(d) > 4999:
+		reportTwoCallback.append('')
+	else:
+		reportTwoCallback.append(comprehend.detect_key_phrases(Text=d,LanguageCode='en')['KeyPhrases'])
+check = 0
+for e in (ASRS_DF['Report_Synopsis']):
+	check +=1
+	if check > 50:
+		break
+	if e == '' or sys.getsizeof(e) > 4999:
+		reportSynopsis.append('')
+	else:
+		reportSynopsis.append(comprehend.detect_key_phrases(Text=e,LanguageCode='en')['KeyPhrases'])
+
+	
+	
+	
 
 
-#convert to list        
-for i in ASRS_DF['Report 1']:
-	print(i)
 
-text = "The weather in boston is pretty trash"
 
-print("Detecting Key Phrases")
-print(json.dumps(comprehend.detect_key_phrases(Text=text,LanguageCode='en'),sort_keys = True,indent=4))
+
+name  = 'keywords.txt'
+file = open(name,'w')
+
+
+
+
+
+for a,b,c,d,e in zip(reportOneNarrative,reportOneCallback,reportTwoNarrative,reportTwoCallback,reportSynopsis):
+	file.write('Report One Narrative ' + json.dumps(a) + 'Report One Callback ' + json.dumps(b) + 'Report Two Narrative ' + json.dumps(c) + 'Report Two Callback ' + json.dumps(d) +  'Report Synopsis ' + json.dumps(e))
+	file.write('\n')
+
+
+file.close()
+
+
+
+
+
 
 
 
