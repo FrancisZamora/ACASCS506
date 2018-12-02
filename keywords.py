1637e361849e3bfe101329fe46bb6c7e2bb3172b
FrancisZamora <franciszamor@gmail.com>
2018-12-02 17:41:45 -0500

    only synopsis keyword detection

--- keywords.py
+++ keywords.py
@@ -35,57 +35,17 @@ ASRS_DF.columns = [c.replace(' ', '_') for c in ASRS_DF.columns]
 
 
 
-reportOneNarrative = []
-reportOneCallback = []
-reportTwoNarrative = []
-reportTwoCallback = []
+
 reportSynopsis = []
 check = 0
 
-for a in (ASRS_DF['Report_1_Narrative']):
+for r in (ASRS_DF['Report_Synopsis']):
 	check +=1
-	if check > 50:
-		break
-	if a == '' or sys.getsizeof(a) > 4999:
-		reportOneNarrative.append('')
-	else:
-		reportOneNarrative.append(comprehend.detect_key_phrases(Text=a,LanguageCode='en')['KeyPhrases'])
-check = 0
-for b in (ASRS_DF['Report_1_Narrative']):
-	check +=1
-	if check > 50:
-		break
-	if b == '' or sys.getsizeof(b) > 4999:
-		reportOneCallback.append('')
-	else:
-		reportOneCallback.append(comprehend.detect_key_phrases(Text=b,LanguageCode='en')['KeyPhrases'])
-check = 0 
-for c in (ASRS_DF['Report_2_Narrative']):
-	check +=1
-	if check > 50:
-		break
-	if c == '' or sys.getsizeof(c) > 4999:
-		reportTwoNarrative.append('')
-	else:
-		reportTwoNarrative.append(comprehend.detect_key_phrases(Text=c,LanguageCode='en')['KeyPhrases'])
-check = 0
-for d in (ASRS_DF['Report_2_Callback']):
-	check +=1
-	if check > 50:
-		break
-	if d == '' or sys.getsizeof(d) > 4999:
-		reportTwoCallback.append('')
-	else:
-		reportTwoCallback.append(comprehend.detect_key_phrases(Text=d,LanguageCode='en')['KeyPhrases'])
-check = 0
-for e in (ASRS_DF['Report_Synopsis']):
-	check +=1
-	if check > 50:
-		break
-	if e == '' or sys.getsizeof(e) > 4999:
+	
+	if r == '' or sys.getsizeof(r) > 4999:
 		reportSynopsis.append('')
 	else:
-		reportSynopsis.append(comprehend.detect_key_phrases(Text=e,LanguageCode='en')['KeyPhrases'])
+		reportSynopsis.append(comprehend.detect_key_phrases(Text=r,LanguageCode='en')['KeyPhrases'])
 
 	
 	
@@ -95,15 +55,17 @@ for e in (ASRS_DF['Report_Synopsis']):
 
 
 
-name  = 'keywords.txt'
+name  = 'synopsiskeywords.txt'
 file = open(name,'w')
 
 
 
 
 
-for a,b,c,d,e in zip(reportOneNarrative,reportOneCallback,reportTwoNarrative,reportTwoCallback,reportSynopsis):
-	file.write('Report One Narrative ' + json.dumps(a) + 'Report One Callback ' + json.dumps(b) + 'Report Two Narrative ' + json.dumps(c) + 'Report Two Callback ' + json.dumps(d) +  'Report Synopsis ' + json.dumps(e))
+for e in zip(reportSynopsis):
+	#file.write('Report One Narrative ' + json.dumps(a) + 'Report One Callback ' + json.dumps(b) + 'Report Two Narrative ' + json.dumps(c) + 'Report Two Callback ' + json.dumps(d) +  'Report Synopsis ' + json.dumps(e))
+	file.write('Report Synopsis ' + json.dumps(e))
+
 	file.write('\n')
 
 
